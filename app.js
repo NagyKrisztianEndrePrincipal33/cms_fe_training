bcf3b808e8e8f96c90650e27b1cb21bbed4b7222
AzureAD\KrisztianNagy <krisztian.nagy@principal33.com>
2021-08-04 23:02:39 +0300

    Rewriting form generation using switch instead of if

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ class CMS extends HTMLElement {
     }
 
     render() {
+        // this.myStorage.setItem(this.dataBaseKey, null);
         console.log("Rendering the CMS!");
         this.form = this.createFormSection(this.tableHeader);
         this.append(this.form);
@@ -33,9 +34,13 @@ class CMS extends HTMLElement {
         this.table.append(this.createTableHeader(this.tableHeader));
         let dataFromDataBase = this.myStorage.getItem(this.dataBaseKey);
         dataFromDataBase = JSON.parse(dataFromDataBase);
-        dataFromDataBase.forEach(element => {
-            this.table.append(this.createTableRow(this.tableHeader, element));
-        });
+        if (!dataFromDataBase) {
+            console.log("Nothing to show");
+        } else {
+            dataFromDataBase.forEach((element) => {
+                this.table.append(this.createTableRow(element));
+            });
+        }
         this.append(this.table);
     }
 
@@ -51,11 +56,11 @@ class CMS extends HTMLElement {
         return tableRow;
     }
 
-    createTableRow(headerNames, data) {
-        const tableRow = document.createElement('tr');
-        tableRow.classList.add('table-body-row');
+    createTableRow(data) {
+        const tableRow = document.createElement("tr");
+        tableRow.classList.add("table-body-row");
         for (let x in data) {
-            const tableCell = document.createElement('td');
+            const tableCell = document.createElement("td");
             tableCell.innerText = data[x];
             tableRow.append(tableCell);
         }
@@ -65,47 +70,53 @@ class CMS extends HTMLElement {
     createFormSection(headerNames) {
         const form = document.createElement("form");
         headerNames.forEach((element) => {
-            if (element === "Sex") {
-                const label = document.createElement("label");
-                label.for = element.replace(/ /g, "");
-                label.innerText = element + ":";
-                const select = document.createElement("select");
-                select.name = element.replace(/ /g, "");
-                select.id = element.replace(/ /g, "");
-                const male = document.createElement("option");
-                male.value = "Male";
-                male.innerText = "Male";
-                const female = document.createElement("option");
-                female.value = "Female";
-                female.innerText = "Female";
-                select.append(male, female);
-                form.append(label, select);
-                return;
+            switch (element) {
+                case "Sex":
+                    {
+                        const label = document.createElement("label");
+                        label.for = element.replace(/ /g, "");
+                        label.innerText = element + ":";
+                        const select = document.createElement("select");
+                        select.name = element.replace(/ /g, "");
+                        select.id = element.replace(/ /g, "");
+                        const male = document.createElement("option");
+                        male.value = "Male";
+                        male.innerText = "Male";
+                        const female = document.createElement("option");
+                        female.value = "Female";
+                        female.innerText = "Female";
+                        select.append(male, female);
+                        form.append(label, select);
+                        break;
+                    }
+                case "Date of birth":
+                    {
+                        const label = document.createElement("label");
+                        label.for = element.replace(/ /g, "");
+                        label.innerText = element + ":";
+                        const input = document.createElement("input");
+                        input.required = true;
+                        input.type = "date";
+                        input.id = element.replace(/ /g, "");
+                        form.append(label, input);
+                        break;
+                    }
+                case "Profile Image":
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        const label = document.createElement("label");
+                        label.for = element.replace(/ /g, "");
+                        label.innerText = element + ":";
+                        const input = document.createElement("input");
+                        input.id = element.replace(/ /g, "");
+                        input.required = true;
+                        input.type = "text";
+                        form.append(label, input);
+                    }
             }
-            if (element === "Date of birth") {
-                const label = document.createElement("label");
-                label.for = element.replace(/ /g, "");
-                label.innerText = element + ":";
-                const input = document.createElement("input");
-                input.required = true;
-                input.type = "date";
-                input.id = element.replace(/ /g, "");
-                form.append(label, input);
-                return;
-            }
-
-            if (element === "Profile Image") {
-                return;
-            }
-
-            const label = document.createElement("label");
-            label.for = element.replace(/ /g, "");
-            label.innerText = element + ":";
-            const input = document.createElement("input");
-            input.id = element.replace(/ /g, "");
-            input.required = true;
-            input.type = "text";
-            form.append(label, input);
         });
         const submit = document.createElement("input");
         submit.type = "submit";
@@ -144,6 +155,9 @@ class CMS extends HTMLElement {
         dataFromDataBase.push(newEmployee);
         this.myStorage.setItem(this.dataBaseKey, JSON.stringify(dataFromDataBase));
         this.form.reset();
+        this.table.append(
+            this.createTableRow(dataFromDataBase[dataFromDataBase.length - 1])
+        );
     }
 
     _getLastId(dataFromDataBase) {
