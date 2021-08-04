37cb2908b097ac9b0e26e07a7cec694a484e8726
AzureAD\KrisztianNagy <krisztian.nagy@principal33.com>
2021-08-04 21:45:47 +0300

    Form and table header added to component

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ class CMS extends HTMLElement {
     constructor() {
         super();
         this.table = null;
+        this.form = null;
+        this.tableHeader = ["Firstname", "Lastname", "Email", "Sex", "Date of birth", "Profile Image"];
     }
 
     connectedCallback() {
@@ -15,6 +17,76 @@ class CMS extends HTMLElement {
 
     render() {
         console.log("Rendering the CMS!");
+        this.table = document.createElement('table');
+        this.table.classList.add('styled-table');
+        this.table.append(this.createTableHeader(this.tableHeader));
+        this.append(this.table);
+        this.form = this.createFormSection(this.tableHeader)
+        this.append(this.form);
+    }
+
+    createTableHeader(headerNames) {
+        const tableRow = document.createElement('tr');
+        tableRow.classList.add('table-header-row');
+        headerNames.forEach(element => {
+            const tableHeader = document.createElement('th');
+            tableHeader.classList.add('table-cell');
+            tableHeader.innerText = element;
+            tableRow.append(tableHeader);
+        });
+        return tableRow;
+    }
+
+    createFormSection(headerNames) {
+        const form = document.createElement('form');
+        headerNames.forEach(element => {
+            if (element === 'Sex') {
+                const label = document.createElement('label');
+                label.for = element.replace(/ /g, "");
+                label.innerText = element + ":"
+                const select = document.createElement('select');
+                select.name = element.replace(/ /g, "");
+                const male = document.createElement('option');
+                male.value = "Male";
+                male.innerText = "Male";
+                const female = document.createElement('option');
+                female.value = "Female";
+                female.innerText = "Female";
+                select.append(male, female);
+                form.append(label, select);
+                return;
+            }
+            if (element === 'Date of birth') {
+                const label = document.createElement('label');
+                label.for = element.replace(/ /g, "");
+                label.innerText = element + ":";
+                const input = document.createElement('input');
+                input.type = 'date';
+                form.append(label, input);
+                return;
+            }
+
+            if (element === 'Profile Image') {
+                return;
+            }
+
+            const label = document.createElement('label');
+            label.for = element.replace(/ /g, "");
+            label.innerText = element + ":";
+            const input = document.createElement('input');
+            input.type = "text";
+            form.append(label, input);
+        });
+        const submit = document.createElement('input');
+        submit.type = "submit";
+        submit.onclick = () => this.saveDataOnClickSubmit();
+        form.onsubmit = () => { return false };
+        form.append(submit);
+        return form;
+    }
+
+    saveDataOnClickSubmit() {
+        console.log("Handling Data Saving");
     }
 }
 
