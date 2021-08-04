8c4e0c2ef2dac8c7f0c40d151cc606748443d112
AzureAD\KrisztianNagy <krisztian.nagy@principal33.com>
2021-08-04 22:29:21 +0300

    Saving new employee data to local storage done

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ class CMS extends HTMLElement {
         super();
         this.table = null;
         this.form = null;
-        this.tableHeader = ["Firstname", "Lastname", "Email", "Sex", "Date of birth", "Profile Image"];
+        this.tableHeader = [
+            "Firstname",
+            "Lastname",
+            "Email",
+            "Sex",
+            "Date of birth",
+            "Profile Image",
+        ];
+        this.myStorage = window.localStorage;
+        this.dataBaseKey = "Employees";
     }
 
     connectedCallback() {
@@ -17,20 +26,20 @@ class CMS extends HTMLElement {
 
     render() {
         console.log("Rendering the CMS!");
-        this.table = document.createElement('table');
-        this.table.classList.add('styled-table');
+        this.table = document.createElement("table");
+        this.table.classList.add("styled-table");
         this.table.append(this.createTableHeader(this.tableHeader));
         this.append(this.table);
-        this.form = this.createFormSection(this.tableHeader)
+        this.form = this.createFormSection(this.tableHeader);
         this.append(this.form);
     }
 
     createTableHeader(headerNames) {
-        const tableRow = document.createElement('tr');
-        tableRow.classList.add('table-header-row');
-        headerNames.forEach(element => {
-            const tableHeader = document.createElement('th');
-            tableHeader.classList.add('table-cell');
+        const tableRow = document.createElement("tr");
+        tableRow.classList.add("table-header-row");
+        headerNames.forEach((element) => {
+            const tableHeader = document.createElement("th");
+            tableHeader.classList.add("table-cell");
             tableHeader.innerText = element;
             tableRow.append(tableHeader);
         });
@@ -38,61 +47,65 @@ class CMS extends HTMLElement {
     }
 
     createFormSection(headerNames) {
-        const form = document.createElement('form');
-        headerNames.forEach(element => {
-            if (element === 'Sex') {
-                const label = document.createElement('label');
+        const form = document.createElement("form");
+        headerNames.forEach((element) => {
+            if (element === "Sex") {
+                const label = document.createElement("label");
                 label.for = element.replace(/ /g, "");
-                label.innerText = element + ":"
-                const select = document.createElement('select');
+                label.innerText = element + ":";
+                const select = document.createElement("select");
                 select.name = element.replace(/ /g, "");
                 select.id = element.replace(/ /g, "");
-                const male = document.createElement('option');
+                const male = document.createElement("option");
                 male.value = "Male";
                 male.innerText = "Male";
-                const female = document.createElement('option');
+                const female = document.createElement("option");
                 female.value = "Female";
                 female.innerText = "Female";
                 select.append(male, female);
                 form.append(label, select);
                 return;
             }
-            if (element === 'Date of birth') {
-                const label = document.createElement('label');
+            if (element === "Date of birth") {
+                const label = document.createElement("label");
                 label.for = element.replace(/ /g, "");
                 label.innerText = element + ":";
-                const input = document.createElement('input');
+                const input = document.createElement("input");
                 input.required = true;
-                input.type = 'date';
+                input.type = "date";
                 input.id = element.replace(/ /g, "");
                 form.append(label, input);
                 return;
             }
 
-            if (element === 'Profile Image') {
+            if (element === "Profile Image") {
                 return;
             }
 
-            const label = document.createElement('label');
+            const label = document.createElement("label");
             label.for = element.replace(/ /g, "");
             label.innerText = element + ":";
-            const input = document.createElement('input');
+            const input = document.createElement("input");
             input.id = element.replace(/ /g, "");
             input.required = true;
             input.type = "text";
             form.append(label, input);
         });
-        const submit = document.createElement('input');
+        const submit = document.createElement("input");
         submit.type = "submit";
         form.onsubmit = (event) => this.saveDataOnClickSubmit(event);
         form.append(submit);
         return form;
     }
 
-    saveDataOnClickSubmit() {
+    saveDataOnClickSubmit(event) {
         event.preventDefault();
         console.log("Handling Data Saving");
-        this.tableHeader.forEach(element => {
+        let dataFromDataBase = this.myStorage.getItem(this.dataBaseKey);
+        dataFromDataBase = JSON.parse(dataFromDataBase);
+        let newEmployee = {};
+        newEmployee["id"] = this._getLastId(dataFromDataBase) + 1;
+        this.tableHeader.forEach((element) => {
             switch (element) {
                 case "Profile Image":
                     {
@@ -101,12 +114,27 @@ class CMS extends HTMLElement {
                     }
                 default:
                     {
-                        console.log(this.form.querySelector(`#${element.replace(/ /g,"")}`).value);
+                        newEmployee[element.replace(/ /g, "")] = this.form.querySelector(
+                            `#${element.replace(/ /g, "")}`
+                        ).value;
+
                         break;
                     }
             }
         });
+        if (!dataFromDataBase) {
+            dataFromDataBase = [];
+        }
+        dataFromDataBase.push(newEmployee);
+        this.myStorage.setItem(this.dataBaseKey, JSON.stringify(dataFromDataBase));
+    }
+
+    _getLastId(dataFromDataBase) {
+        if (!dataFromDataBase) {
+            return 0;
+        }
+        return dataFromDataBase[dataFromDataBase.length - 1].id;
     }
 }
 
-window.customElements.define('cms-component', CMS);
+window.customElements.define("cms-component", CMS);
